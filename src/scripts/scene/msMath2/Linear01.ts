f51b99d2c6d2985b004e6ede2af3a381a87699f7
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2020-02-17 20:17:17 +0900

    Updated 中１　直線

--- src/scripts/scene/msMath2/Linear01.ts
+++ src/scripts/scene/msMath2/Linear01.ts
@@ -2,6 +2,7 @@
 import SceneBase from '~/scripts/scene/SceneBase';
 import { sShape, sCoord, sColor } from '~/scripts/system';
 import { Line, Circle, Text } from '~/scripts/node/shape';
+import { GUI } from '~/scripts/helper';
 
 /******************************************************************************
  * 直線 y=ax+bのシーン
@@ -10,6 +11,8 @@ export default class Scene extends SceneBase
 {  
   constructor() {
     super();
+    this.onDragMovePointX = this.onDragMovePointX.bind(this);
+    this.onDragMoveTangentY = this.onDragMoveTangentY.bind(this);
   }
 
   //---------------------------------------------------------------------------
@@ -33,18 +36,18 @@ $y=ax+b$
   params = {
     a:1,
     b:0,
-    x:1,
+    x:3,
     y:1,
   }
 
   initGUI() {
     const f1 = this.gui.addFolder("１次関数のパラメータ");
-    f1.add(this.params, "a").step(0.1);
-    f1.add(this.params, "b").step(0.1);
+    GUI.addS10(f1, this.params, "a");
+    GUI.addSTD(f1, this.params, "b");
     f1.open();
 
     const f2 = this.gui.addFolder("xに対するyの値");
-    f2.add(this.params, "x").step(0.1);
+    GUI.addSLR(f2,  this.params, "x");
     f2.add(this.params, "y").step(0.1).listen();
     f2.open();
   }
@@ -60,20 +63,33 @@ $y=ax+b$
   /** グラフ内の要素 */
   private lineLinear:Line        = sShape.solidLine().points([0, 0, 1, 1]);
   private lineX2Y:Line           = sShape.brokenLine();
-  private pointInterceptY:Circle = sShape.point();
-  private pointX:Circle          = sShape.point().fill(sColor.red);
-  private pointY:Circle          = sShape.point().fill(sColor.red);
+  private pointTangentY:Circle = sShape.draggablePoint();
+  private pointX:Circle          = sShape.draggablePoint().fill(sColor.red);
+  private pointY:Circle          = sShape.point();
   private coordX:Text            = sShape.text().offset(0.1, -0.1);
-  private coordY:Text            = sShape.text().offset(0.1, -0.1);
+  private coordY:Text            = sShape.text().offset(-1.1, -0.1);
+  private coordB:Text            = sShape.text().offset(0.1, -0.1);
 
   initGraph() {
+    this.pointX.on('dragmove', this.onDragMovePointX);
+    this.pointTangentY.on('dragmove', this.onDragMoveTangentY);
+
     this.add(this.lineLinear);
-    this.add(this.pointInterceptY);
-    this.add(this.pointX);
     this.add(this.lineX2Y);
-    this.add(this.pointY);
+    this.add(this.pointTangentY);
     this.add(this.coordX);
     this.add(this.coordY);
+    this.add(this.pointX);
+    this.add(this.pointY);
+    this.add(this.coordB);
+  }
+
+  onDragMovePointX(e:Circle) {
+    this.params.x = e.x();
+  }
+
+  onDragMoveTangentY(e:Circle) {
+    this.params.b = e.y();
   }
 
   update() {
@@ -87,17 +103,18 @@ $y=ax+b$
     this.lineLinear.points([sCoord.left, y1, sCoord.right, y2]);
 
     // Y切片の位置更新(緑丸)
-    this.pointInterceptY.pos(0, b);
+    this.pointTangentY.pos(0, b);
 
     // X点、Y点(赤丸)
     this.pointX.pos(x, 0);
-    this.pointY.pos(x, y);
+    this.pointY.pos(0, y);
 
     // X -> Y の破線
     this.lineX2Y.points([x, 0, x, y, 0, y]);
 
     // X座標、Y座標(text)
-    this.coordX.pos(x, 0).text(x.toFixed(1));
-    this.coordY.pos(x, y).text(y.toFixed(1));
+    this.coordX.pos(x, 0).text("x=" + x.toFixed(1));
+    this.coordY.pos(0, y).text("y=" + y.toFixed(1));
+    this.coordB.pos(0, b).text("b=" + b.toFixed(1));
   }
 }
